5e1e05ffa4776b3a56d7916b67643c76e3faaf8c
tarar290190 <68437112+tarar290190@users.noreply.github.com>
2020-07-20 14:55:49 +0530

    Add files via upload

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Models;
+using Newtonsoft.Json;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserData _userData;
+
+        public UserController(IUserData userData)
+        {
+            this._userData = userData;
+        }
+        // GET: api/User
+
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return JsonConvert.SerializeObject(_userData.GetById(id));
+        }
+
+
+    }
+}
